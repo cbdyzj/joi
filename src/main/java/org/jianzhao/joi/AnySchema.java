be2f8dde9751556a34ef0c0e49206c68bbc5d9b6
Cbdy <cbdyzj@jianzhao.org>
2018-03-10 22:56:32 +0800

    update AnySchema.java

--- src/main/java/org/jianzhao/joi/AnySchema.java
+++ src/main/java/org/jianzhao/joi/AnySchema.java
@@ -10,23 +10,25 @@ import java.util.function.Supplier;
 @SuppressWarnings("unchecked")
 public abstract class AnySchema<T, S extends AnySchema<T, ?>> implements Schema<T> {
 
-    private static final String DEFAULT_MESSAGE = "Validate failed!";
+    private static final Supplier<Optional<String>> DEFAULT_MESSAGE = () -> Optional.of("Validate failed!");
 
-    private Supplier<Optional<String>> message = () -> Optional.of(DEFAULT_MESSAGE);
-    private Supplier<Optional<String>> requiredMessage = Optional::empty;
-
+    private Supplier<Optional<String>> message = Optional::empty;
     private List<Schema<T>> schemas = new ArrayList<>();
+    private boolean required = false;
 
     @Override
     public Optional<String> validate(T target) {
         // target is null
         if (Objects.isNull(target)) {
-            return this.requiredMessage.get();
+            return this.validateNull();
         }
         // target is not null
-        Optional<String> message = this.schemas.stream().map(schema -> schema.validate(target)).filter(Optional::isPresent).reduce(Optional.empty(), (a, c) -> c);
-        if (message.isPresent() && this.message.get().isPresent() && !DEFAULT_MESSAGE.equals(this.message.get().get())) {
-            message = this.message.get();
+        Optional<String> message = this.schemas.stream()
+                .map(schema -> schema.validate(target))
+                .filter(Optional::isPresent)
+                .reduce(Optional.empty(), (a, c) -> c);
+        if (message.isPresent() && this.message.get().isPresent()) {
+            return this.message.get();
         }
         return message;
     }
@@ -37,7 +39,7 @@ public abstract class AnySchema<T, S extends AnySchema<T, ?>> implements Schema<
     }
 
     public S required() {
-        this.requiredMessage = () -> Optional.of(DEFAULT_MESSAGE);
+        this.required = true;
         return (S) this;
     }
 
@@ -47,7 +49,19 @@ public abstract class AnySchema<T, S extends AnySchema<T, ?>> implements Schema<
     }
 
     public S predicate(Predicate<T> predicate) {
-        this.schemas.add(target -> predicate.test(target) ? Optional.empty() : this.message.get());
+        this.schemas.add(target -> predicate.test(target) ? Optional.empty() : DEFAULT_MESSAGE.get());
         return (S) this;
+    }
+
+    private Optional<String> validateNull(){
+        if(this.required){
+            if(this.message.get().isPresent()){
+                return this.message.get();
+            }else {
+                return DEFAULT_MESSAGE.get();
+            }
+        }else {
+            return Optional.empty();
+        }
     }
 }
