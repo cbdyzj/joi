8a2eb7cce8cbc1aa2dd398e1a1d63c7bbfbecec1
cbdyzj <cbdyzj@jianzhao.org>
2018-04-12 21:45:06 +0800

    Update Schema interface

--- src/main/java/org/jianzhao/joi/Schema.java
+++ src/main/java/org/jianzhao/joi/Schema.java
@@ -3,4 +3,11 @@ package org.jianzhao.joi;
 public interface Schema<T> {
 
     Result validate(T target);
+
+    default Schema<T> compose(Schema<T> after) {
+        return target -> {
+            Result rb = this.validate(target);
+            return rb.isInvalid() ? rb : after.validate(target);
+        };
+    }
 }
